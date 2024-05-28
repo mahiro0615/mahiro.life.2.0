60baf0037f9d252492de8b05424513b428d2a103
mahiro0615 <yamakawa.m@northeastern.edu>
2024-05-28 10:07:38 -0400

    Enable comment section

--- src/data/siteMetadata.js
+++ src/data/siteMetadata.js
@@ -4,25 +4,21 @@ const siteMetadata = {
   author: 'Your Name',
  
 comments: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // Select a provider and use the environment variables associated to it
-    // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    
+    provider: 'giscus', 
     giscusConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
+   
+      repo: 'mahiro0615/mahiro.life.2.0',
+      repositoryId: "R_kgDOMBsy3g",
+      category: 'General',
+      categoryId: "DIC_kwDOMBsy3s4CfqXd",
+      mapping: 'pathname', 
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
       metadata: '0',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
+      theme: "noborder_light",
       // theme when dark mode
       darkTheme: 'transparent_dark',
       // If the theme option above is set to 'custom`
