b85b0eafef6b8a0652cbc2dfa5c713c5f49a591e
mahiro0615 <yamakawa.m@northeastern.edu>
2024-05-28 08:43:18 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}",
+  "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./posts/**/*.{js,ts,jsx,tsx,md,mdx}",
+
+],
+
+  theme: {
+    extend: {
+      colors: {
+        // Custom text color
+        text: '#22454', // Change this to your desired color value
+        background: '#fffaf2', 
+      },
+      fontFamily: {
+        Default: ['verdana', 'sans-serif'], // Custom font family
+        Geo: ['Georgia', 'sans-serif'],
+        Hel: ['Helvetica', 'sans-serif'],
+      },
+
+      
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@headlessui/tailwindcss'),
+  ],
+};
+
