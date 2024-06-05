11e1a9523774eb85411835067cbc74561aa8ed93
mahiro0615 <yamakawa.m@northeastern.edu>
2024-06-06 06:18:45 +0900

    new section

--- src/app/(sub-pages)/projects/page.jsx
+++ src/app/(sub-pages)/projects/page.jsx
@@ -1,34 +1,50 @@
 import { Content } from "@/components/content";
 import { Header } from "@/components/header";
-
-
-
+import Link from 'next/link';
+import path from 'path';
+import fs from 'fs/promises';
 
 export const metadata = {
-  title: "Collab",
-  description:
-    "Lets collab and by collab, I mean lets be in each other's life",
-
+  title: "Projects",
+  description: "Things I am working on",
   alternates: {
     canonical: `/projects`,
   },
 };
 
-export default async function projectPage () {
-    return (
-        <div className="space-y-8">
+async function getProjects() {
+  const projectsDirectory = path.join(process.cwd(), 'projects');
+  try {
+    const files = await fs.readdir(projectsDirectory);
+    return files.map(file => file.replace('.mdx', ''));
+  } catch (error) {
+    console.error('Error reading projects directory:', error);
+    return [];
+  }
+}
+
+export default async function ProjectPage() {
+  const projects = await getProjects();
+
+  return (
+    <div className="space-y-8">
       <Header>
-      <h1 className = 'block mb-6 text-2xl font-bold'> Projects</h1>
-          
-        <p className = 'font-Default'>
-         Coming Soon...
+        <h1 className='block mb-6 text-2xl font-bold'>Projects</h1>
+        <p className='font-Default'>
+          Things I am working on
         </p>
-      
       </Header>
       <Content>
-
+        <ul>
+          {projects.map((project) => (
+            <li key={project}>
+              <Link href={`/project/${project}`}>
+                {project}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </Content>
-      </div>
-        
-    );
+    </div>
+  );
 }
